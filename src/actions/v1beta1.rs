9aa8c5a19a30de816df0d1b2c815332fa0ef1c25
Zohar Zilberman <popen2@gmail.com>
2023-05-13 17:45:42 +0300

    Fix ChartExtActionsV1Beta1 to be a tuple struct

--- src/actions/v1beta1.rs
+++ src/actions/v1beta1.rs
@@ -4,14 +4,11 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
 #[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
-#[serde(transparent)]
-pub struct ChartExtActionsV1Beta1 {
-    pub actions: Vec<ChartExtActionV1Beta1>,
-}
+pub struct ChartExtActionsV1Beta1(Vec<ChartExtActionV1Beta1>);
 
 impl ChartExtActionsV1Beta1 {
     pub fn find(&self, action_id: &str) -> Option<&ChartExtActionV0> {
-        self.actions
+        self.0
             .iter()
             .find(|action| action.spec.id == action_id)
             .map(|action| &action.spec)
